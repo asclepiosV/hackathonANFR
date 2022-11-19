eac0c9746bad731e728a98e6e2a12ad891ccf067
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-19 16:54:00 +0100

    add map fr

--- front/src/components/map/Map.jsx
+++ front/src/components/map/Map.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import France from "@svg-maps/france.departments";
-import CheckboxSVGMap from "./checkbox-svg-map";
+import SVGMap from "./svg-map";
 import './map.css';
+import {useNavigate} from "react-router-dom";
 
 
+export default function Map(){
+    const navigate = useNavigate();
+    const getLocation = event => {
+        navigate('/departement/' + event.target.attributes['id'].nodeValue)
+    }
 
-function Map() {
     return (
         <div className={"map"}>
-            <CheckboxSVGMap map={France} />
+            <SVGMap map={France} onLocationClick={getLocation}/>
         </div>
     );
 }
-
-export default Map;
