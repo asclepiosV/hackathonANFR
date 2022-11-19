f2c6e6171f4d48b0482f164223227cda8b152640
Tugdual de Kerdrel <tugdualk@hotmail.com>
2022-11-19 14:19:10 +0100

    header

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
-import 'header.css'
+import './header.css'
+import logo from '../../assets/logo.svg'
 
 function Header() {
     return (
-        <div></div>
+        <div>
+            <header className='navBar'>
+                <div className="logo-container">
+                    <img src={logo} />
+                </div>
+                <ul className="nav-list" id="nav-list">
+                    <li className="list-item">
+                        <a href="#">Map</a>
+                    </li>
+                    <li className="list-item">
+                        <a href="#">Explanations</a>
+                    </li>
+                </ul>
+                <div className="menu" id="toggle-button" onclick="toggleNav()">
+                    <div className="menu-line-1"></div>
+                    <div className="menu-line-2"></div>
+                    <div className="menu-line-3"></div>
+                </div>
+            </header>
+        </div>
     );
 }
 
