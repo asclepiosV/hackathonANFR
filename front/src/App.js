6f5c8abc2b975b8fecaf532114645a78a10c58c0
Tugdual de Kerdrel <tugdualk@hotmail.com>
2022-11-19 16:54:58 +0100

    chart

--- front/src/App.js
+++ front/src/App.js
@@ -1,19 +1,20 @@
-import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import Map from "./components/map/Map"
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Map from "./components/map/Map";
 import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer"
-
+import Footer from "./components/footer/Footer";
+import Region from "./components/chart/ChartBande";
 
 function App() {
   return (
-      <Router>
-        <Header/>
-        <Routes>
-          <Route path={"/"} element={<Map/>}/>
-        </Routes>
-        <Footer />
-      </Router>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path={"/"} element={<Map />} />
+        <Route path={"/region"} element={<Region />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
