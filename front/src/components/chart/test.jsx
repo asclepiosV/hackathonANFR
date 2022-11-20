7473733fa25508c86a28f61d76c191b6de7fa0aa
Tugdual de Kerdrel <tugdualk@hotmail.com>
2022-11-20 14:46:41 +0100

    final

---
+++ front/src/components/chart/test.jsx
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import Plot from 'react-plotly.js';
+import fakeData from "../../data/fakeData2.json";
+import ReactDOM from 'react-dom'; 
+
+console.log(fakeData);
+
+class BarChart extends Component{
+    constructor(){
+        use this.state;
+    }
+    
+    render(){
+        console.log({data : this.props.route.params.data}.map( item => { return item.ID_INTERV_FREQ }));
+        return(
+            <div>
+                <Plot 
+                    data={[
+                        {type: 'bar',
+                        x: {data : this.props.route.params.data}.map( item => { return item.ID_INTERV_FREQ }),
+                        y: {data : this.props.route.params.data}.map( item => { return item.Number }),
+                    }
+                    ]}
+                    layout={{width:1000, height: 500, title:'test'}}
+                    onClick={() => console.debug("onClick")}
+                />
+            </div>
+        )
+    }
+}
+
+export default BarChart;
