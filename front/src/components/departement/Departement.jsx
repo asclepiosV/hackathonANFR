510b46f16534ad8f762ebaa423bb866f5aecb0c8
Paul Flammarion <pa.flammarion@gmail.com>
2022-11-20 12:14:31 +0100

    dept

--- front/src/components/departement/Departement.jsx
+++ front/src/components/departement/Departement.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
-import {useNavigate} from "react-router-dom";
-import ChartBand from "../chart/ChartBande"
+import React, {useEffect, useState} from 'react';
+import {useNavigate, useParams} from "react-router-dom";
+import DepartementService from "../../services/departement.service";
+import Loader from "../loader/Loader";
+import Plot from "react-plotly.js";
+import './departement.css'
 
 
 export default function Map(){
+    const [dataset,setDataset]= useState(null);
+    let deptId  = useParams().id;
+    const getData=()=>{
+        DepartementService.getDepartementChartById(deptId, 0)
+            .then(function(response){
+                return response;
+            })
+            .then(function(myJson) {
+                setDataset(myJson.data)
+            });
+    }
+    useEffect(()=>{
+        getData()
+    },[])
 
     return (
         <div>
             <h1>Région : </h1>
-            <ChartBand />
+            { dataset ?
+                <Plot
+                    data={[
+                        {type: 'bar',
+                            x: dataset.map( item => { return item.ID_INTERV_FREQ }),
+                            y: dataset.map( item => { return item.Number }),
+                        }
+                    ]}
+                layout={{width:1000, height: 500, title:'Enregistrements'}}
+                onClick={() => console.debug("onClick")}
+                />
+            : <Loader />}
         </div>
     );
 }
--- front/src/services/departement.service.js
+++ front/src/services/departement.service.js
@@ -3,15 +3,14 @@ import axios from "axios";
 
 
 const ip = "192.168.0.100"
-const API_URL = "http://"+ ip+ ":5000/api/departement/";
+const API_URL = "http://"+ ip+ ":5000/api/";
 
 const getDepartementChartById = async (id, tel) => {
     try {
-        return await axios.get(API_URL + "0" + id + "/" + tel);
+        return await axios.get(API_URL + "departement/0" + id + "/" + tel);
     } catch (err) {
         console.error(err);
     }
-
 };
 
 const DepartementService = {
